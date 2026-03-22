5f08082ab52c3d14e749ec16a6f9c9a5073414e7
Local Developer <projects@local.dev>
2026-03-22 18:04:44 +0530

    Initial commit

---
+++ Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObjectsX : MonoBehaviour
+{
+    public float spinSpeed;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.RotateAround(transform.position, Vector3.up, spinSpeed * Time.deltaTime);
+
+    }
+}
